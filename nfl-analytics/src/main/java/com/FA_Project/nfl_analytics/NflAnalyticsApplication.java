2d38be50b1f86690c6faaa0615659b16e600632e
Nystu <nathaniellewisnystuen@outlook.com>
2025-04-17 00:10:36 -0500

    Add Spring Boot backend project to Backend branch

---
+++ nfl-analytics/src/main/java/com/FA_Project/nfl_analytics/NflAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.FA_Project.nfl_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NflAnalyticsApplication.class, args);
+	}
+
+}
---
+++ nfl-analytics/src/test/java/com/FA_Project/nfl_analytics/NflAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FA_Project.nfl_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NflAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
